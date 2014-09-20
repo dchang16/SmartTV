99a4f81908ba408e313b1dbc2b7fb1ec98faaecc
David Chang <davidchang@v1020-wn-29-66.campus-dynamic.uwaterloo.ca>
2014-09-20 18:55:03 -0400

    working as intended... needs improvements

--- openelec.lua
+++ openelec.lua
@@ -1,5 +1,9 @@
 scriptId = 'com.thalmic.openelec'
 
+local check = false
+local counter = 0
+local centre = 0
+
 function conditionallySwapWave(pose)
     if myo.getArm() == "left" then
         if pose == "waveIn" then
@@ -12,68 +16,97 @@ function conditionallySwapWave(pose)
 end
 
 function unlock()
-	unlocked = true;
+    unlocked = true;
 end
 
 function lock()
-	unlocked = false;
+    unlocked = false;
 end
 
 function onPoseEdge(pose, edge)
 
     pose = conditionallySwapWave(pose)
 
-    if pose == "thumbToPinky" then
-        if not unlocked then
-        	if edge == "off" then
-            -- Unlock when pose is released in case the user holds it for a while.
-           		unlock()
-           		myo.debug("ON")
-           	end
-        elseif unlocked then
-        	if edge == "off" then
-            	-- Vibrate twice on unlock.
-            	-- We do this when the pose is made for better feedback.
-            	myo.vibrate("short")
-            	myo.vibrate("short")
-            	lock()
-            	myo.debug("OFF")
-            end
-        end
-    end
+    -- if pose == "thumbToPinky" then
+    --     if not unlocked then
+    --      if edge == "off" then
+    --         -- Unlock when pose is released in case the user holds it for a while.
+    --              unlock()
+    --              myo.debug("ON")
+    --          end
+    --     elseif unlocked then
+    --      if edge == "off" then
+    --          -- Vibrate twice on unlock.
+    --          -- We do this when the pose is made for better feedback.
+    --          myo.vibrate("short")
+    --          myo.vibrate("short")
+    --          lock()
+    --          myo.debug("OFF")
+    --         end
+    --     end
+    -- end
 
     -- Other poses only when active
         -- Left key
     if unlock then
-	    if pose == "waveIn" and edge == "on" then
-	        myo.vibrate("short")
-	        myo.keyboard("left_arrow","press")
-	        myo.debug("LEFT")
-	    end
+        if pose == "waveIn" and edge == "on" then
+            myo.vibrate("short")
+            myo.keyboard("left_arrow","press")
+            myo.debug("LEFT")
+        end
 
-	        -- Right key
-	    if pose == "waveOut" and edge == "on" then
-	        myo.keyboard("right_arrow","press")
-	        myo.debug("RIGHT")
-	    end
+            -- Right key
+        if pose == "waveOut" and edge == "on" then
+            myo.keyboard("right_arrow","press")
+            myo.debug("RIGHT")
+        end
 
-	    if pose == "fingersSpread" and edge == "on" then
-	    	myo.keyboard("return", "press")
-	     	myo.debug("RETURN")
-	    end
+        if pose == "fingersSpread" and edge == "on" then
+            myo.keyboard("return", "press")
+            myo.debug("RETURN")
+        end
 
 
-	    if pose == "fist" and edge == "on" then
-	     	myo.keyboard("escape", "press")
-	      	myo.debug("ESCAPE")
-	    end
-	end
+        if (pose == "fist" or pose == "thumbToPinky") then
+            if edge == "on" then -- clicks down to drag
+                check = true
+                myo.debug(myo.getRoll())
+                centre = myo.getRoll()
+             elseif edge == "off" and counter <= 15 then
+                counter = 0
+                check = false
+                myo.keyboard("backspace", "press")
+                myo.debug("BACKSPACE")     
+             elseif edge == "off" then -- release click
+                check = false   
+            end
+        end
+        
+    end
 
 end
 
 -- Other callbacks
 
+
+
 function onPeriodic()
+
+    if check == true then
+        myo.debug(myo.getRoll())
+        counter = counter + 1
+    end
+
+    if counter > 15 then
+      if myo.getRoll() > (centre + 0.3) then
+        myo.keyboard("up_arrow", "press")
+        myo.debug("UP")
+      elseif myo.getRoll() < (centre - 0.3) then
+        myo.keyboard("down_arrow", "press")
+        myo.debug("DOWN")
+      end
+    counter = 0
+    end
 end
 
 function onForegroundWindowChange(app, title)
